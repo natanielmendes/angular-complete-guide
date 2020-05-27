c3b9a124d2e9e7a5baf014c08876ecac8c6f621f
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-05-27 17:02:41 -0300

    Manipulating the State

--- src/App.js
+++ src/App.js
@@ -5,10 +5,22 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      { name: 'Fulano', age: Math.floor(Math.random() * 21) },
+      { name: 'Fulano', age: Math.floor(Math.random() * 7) + 23 },
       { name: 'Cicrano', age: 22 },
       { name: 'CKAD', age: 23 }
     ]
+  }
+
+  switchNameHandler = () => {
+    // console.log("Was clicked!")
+    // DON'T DO THIS! this.state.persons[0].name = 'Natan'
+    this.setState({
+      persons: [
+        { name: 'Natan', age: Math.floor(Math.random() * 7) + 23 },
+        { name: 'Cicrano', age: Math.floor(Math.random() * 12) + 18 },
+        { name: 'CKAD', age: 23 }
+      ]
+    })
   }
 
   render() {
@@ -16,7 +28,7 @@ class App extends Component {
       <div className="App">
         <h1 className="App-title">Hi, I'm a React App</h1>
         <p>This is really working! YAY</p>
-        <button>Switch Name</button>
+        <button onClick={this.switchNameHandler}>Switch Name</button>
         <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
         <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
         <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
