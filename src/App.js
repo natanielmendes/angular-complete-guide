b9af3258596f9fe5971b3907a194269b52d88bde
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-05-27 17:31:48 -0300

    Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -1,20 +1,24 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Person from './Person/Person';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ personsState, setPersonsState ] = useState({
     persons: [
       { name: 'Fulano', age: Math.floor(Math.random() * 7) + 23 },
-      { name: 'Cicrano', age: 22 },
+      { name: 'Cicrano', age: Math.floor(Math.random() * 12) + 18 },
       { name: 'CKAD', age: 23 }
     ]
-  }
+  });
 
-  switchNameHandler = () => {
+  const [otherState, setOtherState ] = useState('some other value');
+
+  console.log(personsState, otherState);
+
+  const switchNameHandler = () => {
     // console.log("Was clicked!")
     // DON'T DO THIS! this.state.persons[0].name = 'Natan'
-    this.setState({
+    setPersonsState({
       persons: [
         { name: 'Natan', age: Math.floor(Math.random() * 7) + 23 },
         { name: 'Cicrano', age: Math.floor(Math.random() * 12) + 18 },
@@ -23,18 +27,36 @@ class App extends Component {
     })
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1 className="App-title">Hi, I'm a React App</h1>
-        <p>This is really working! YAY</p>
-        <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
-        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My hobbies: Racing</Person>
-        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1 className="App-title">Hi, I'm a React App</h1>
+      <p>This is really working! YAY</p>
+      <button onClick={switchNameHandler}>Switch Name</button>
+      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Racing</Person>
+      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+    </div>
+  );
 }
 
-export default App;
+export default app;
+
+// state = {
+//   persons: [
+//     { name: 'Fulano', age: Math.floor(Math.random() * 7) + 23 },
+//     { name: 'Cicrano', age: 22 },
+//     { name: 'CKAD', age: 23 }
+//   ]
+// }
+
+// switchNameHandler = () => {
+//   // console.log("Was clicked!")
+//   // DON'T DO THIS! this.state.persons[0].name = 'Natan'
+//   this.setState({
+//     persons: [
+//       { name: 'Natan', age: Math.floor(Math.random() * 7) + 23 },
+//       { name: 'Cicrano', age: Math.floor(Math.random() * 12) + 18 },
+//       { name: 'CKAD', age: 23 }
+//     ]
+//   })
+// }
