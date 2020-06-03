390dc5b963a6c6cb7cb2d101f86e049cb268519b
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-03 17:45:08 -0300

    Passing Method References Between Components

--- src/App.js
+++ src/App.js
@@ -15,12 +15,12 @@ const app = props => {
 
   console.log(personsState, otherState);
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     // console.log("Was clicked!")
     // DON'T DO THIS! this.state.persons[0].name = 'Natan'
     setPersonsState({
       persons: [
-        { name: 'Natan', age: Math.floor(Math.random() * 7) + 23 },
+        { name: newName, age: Math.floor(Math.random() * 7) + 23 },
         { name: 'Cicrano', age: Math.floor(Math.random() * 12) + 18 },
         { name: 'CKAD', age: 23 }
       ]
@@ -31,14 +31,16 @@ const app = props => {
     <div className="App">
       <h1 className="App-title">Hi, I'm a React App</h1>
       <p>This is really working! YAY</p>
-      <button onClick={switchNameHandler}>Switch Name</button>
+      <button onClick={() => switchNameHandler('Nataniel!!')}>Switch Name</button> {/* React can re-render certain things
+      too often using this inneficient approach */}
       <Person 
         name={personsState.persons[0].name}
         age={personsState.persons[0].age}/>
       <Person
         name={personsState.persons[1].name}
         age={personsState.persons[1].age}
-        click={switchNameHandler}>My hobbies: Racing</Person>
+        click={switchNameHandler.bind(this, 'Natan!')}>My hobbies: Racing</Person> {/* Preferably use this approach in order
+        to bind arguments to the function */}
       <Person 
         name={personsState.persons[2].name} 
         age={personsState.persons[2].age}/>
