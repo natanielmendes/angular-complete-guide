2c02ebb88b96b6d462a2a694e05c96da7f324665
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-03 16:32:27 -0300

    Using the useState() Hook for State Manipulation

--- src/App.js
+++ src/App.js
@@ -32,9 +32,16 @@ const app = props => {
       <h1 className="App-title">Hi, I'm a React App</h1>
       <p>This is really working! YAY</p>
       <button onClick={switchNameHandler}>Switch Name</button>
-      <Person name={personsState.persons[0].name} age={personsState.persons[0].age}/>
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age}>My hobbies: Racing</Person>
-      <Person name={personsState.persons[2].name} age={personsState.persons[2].age}/>
+      <Person 
+        name={personsState.persons[0].name}
+        age={personsState.persons[0].age}/>
+      <Person
+        name={personsState.persons[1].name}
+        age={personsState.persons[1].age}
+        click={switchNameHandler}>My hobbies: Racing</Person>
+      <Person 
+        name={personsState.persons[2].name} 
+        age={personsState.persons[2].age}/>
     </div>
   );
 }
