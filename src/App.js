7f5ca8a0afc2aaa756a802a3e876bf9a19fbd3d9
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-15 19:04:16 -0300

    Flexible Lists

--- src/App.js
+++ src/App.js
@@ -18,6 +18,21 @@ class App extends Component {
       showPersons: !this.state.showPersons
     })
   }
+
+  nameChangedHandler = (event, id) => {
+    const personIndex = this.state.persons.findIndex(p => {
+      return p.id === id;
+    });
+
+    const person = {...this.state.persons[personIndex]};
+
+    person.name = event.target.value;
+
+    const persons = [...this.state.persons];
+    persons[personIndex] = person;
+
+    this.setState({persons: persons});
+  }
   
   deletePersonHandler = (personIndex) => {
     // const persons = this.state.persons; <-- WRONG
@@ -45,7 +60,8 @@ class App extends Component {
               click={() => this.deletePersonHandler(index)}
               name={person.name}
               age={person.age}
-              key={person.id} />
+              key={person.id}
+              changed={(event) => this.nameChangedHandler(event, person.id)} />
           })}
         </div>
       )
