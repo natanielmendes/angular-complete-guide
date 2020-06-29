2905253a925dd1c701b94d4043b882aa342bc282
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-06-29 18:50:57 -0300

    Class-based vs Functional Components

--- src/containers/App.js
+++ src/containers/App.js
@@ -57,6 +57,7 @@ class App extends Component {
     return (
       <div className="App">
         <Cockpit 
+          title={this.props.appTitle}
           showPersons={this.state.showPersons} 
           persons={this.state.persons}
           clicked={this.togglePersonsHandler} />
