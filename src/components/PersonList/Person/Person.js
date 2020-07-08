3824efe814d02613bad8a1c0ad664bbff454913a
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-07-08 08:56:02 -0300

    From Auxiliary to Aux inside Person.js file

--- src/components/PersonList/Person/Person.js
+++ src/components/PersonList/Person/Person.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-import Auxiliary from '../../../hoc/Auxiliary';
+import Aux from '../../../hoc/Auxiliary';
 import './Person.css';
 
 class Person extends Component {
@@ -11,11 +11,11 @@ class Person extends Component {
     render() {
         console.log('[Person.js] rendering...')
         return (
-            <Auxiliary>
+            <Aux>
                 <p key="i1" onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>
                 <p key="i2">{this.props.children}</p>
                 <input key="i3" type="text" onChange={this.props.changed} value={this.props.name}></input>
-            </Auxiliary>
+            </Aux>
         );
     }
 }
