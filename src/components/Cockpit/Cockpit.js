200869cd767d1731afa87b1a996c9ce3ea0e2d03
Nataniel Carvalho <natanielmendes@hotmail.com>
2020-07-12 20:44:07 -0300

    contextType & useContext()

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,10 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef, useContext } from 'react';
 
 import classes from './Cockpit.css';
 import AuthContext from '../../context/auth-context';
 
 const cockpit = (props) => {
     const toggleBtnRef = useRef(null);
+    const authContext = useContext(AuthContext);
+
+    console.log(authContext.authenticated)
 
     useEffect(() => {
         console.log('[Cockpit.js] useEffect');
@@ -43,9 +46,7 @@ const cockpit = (props) => {
             <h1>{props.title}</h1>
             <p className={assignedClasses.join(' ')}>This is really working! YAY</p>
             <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>Toggle Person List</button>
-            <AuthContext.Consumer>
-                {(context) => <button onClick={context.login}>Log in</button>}
-            </AuthContext.Consumer>
+            {<button onClick={authContext.login}>Log in</button>}
         </div>
     );
 };
--- src/components/PersonList/Person/Person.js
+++ src/components/PersonList/Person/Person.js
@@ -12,9 +12,12 @@ class Person extends Component {
         this.inputElementRef = React.createRef();
     }
 
+    static contextType = AuthContext;
+
     componentDidMount() {
         // this.inputElement.focus();
         this.inputElementRef.current.focus();
+        console.log(this.context.authenticated)
     }
 
     componentDidUpdate() {
@@ -25,9 +28,7 @@ class Person extends Component {
         console.log('[Person.js] rendering...')
         return (
             <Aux>
-                <AuthContext.Consumer>
-                    {context => context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
-                </AuthContext.Consumer>
+                {this.context.authenticated ? <p>Authenticated!</p> : <p>Please log in</p>}
                 <p onClick={this.props.click}>I'm {this.props.name} and I am {this.props.age} years old.</p>
                 <p>{this.props.children}</p>
                 <input 
